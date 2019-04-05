ce4d79957b45b70e24c30b4847eaa55b03c3afaf
mariusz.drobot <mariusz.drobot@accenture.com>
2019-04-05 09:21:18 +0200

    'test'

--- src/com/Example2.groovy
+++ src/com/Example2.groovy
@@ -1,12 +1,41 @@
 package com
 
+import com.michelin.cio.hudson.plugins.rolestrategy.*
+import hudson.security.Permission
+import jenkins.model.Jenkins
+
 class Example2 {
 
-    String getString(Script script) {
-        script.echo("Czesc swiat");
+    private RoleBasedAuthorizationStrategy roleBasedAuthorizationStrategy;
 
+    final Map<String, Permission> GLOBAL_ROLES_PERMISSIONS = [
+            'Administer': Jenkins.ADMINISTER,
+            'Read': Jenkins.READ
+    ]
+
+    String getString(Script script) {
+        script.echo("Czesc swiat")
         return "example2"
     }
+
+    void getRoles(String userId= "User", String type = GLOBAL_ROLES_TYPE, Script script) {
+        final Set<Role> result = [] as Set
+
+        final RoleMap roleMap = roleBasedAuthorizationStrategy.getRoleMap(type);
+        final Set<Role> roles = roleMap.getRoles();
+
+        if (userId) {
+            roles.each { role ->
+                final Set<String> sids = roleMap.getSidsForRole(role.getName())
+                if (sids.contains(userId)) {
+                    result.add(role)
+                    script.echo(role)
+                }
+            }
+        }
+
+    }
+
 
 
 }
