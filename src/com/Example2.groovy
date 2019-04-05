c29a62a32690dbbf757c89b631afa267f0d1a6dc
mariusz.drobot <mariusz.drobot@accenture.com>
2019-04-05 09:12:15 +0200

    'test'

--- src/com/Example2.groovy
+++ src/com/Example2.groovy
@@ -1,23 +1,12 @@
 package com
 
-import jenkins.model.Jenkins
-
 class Example2 {
 
-    String getString() {
+    String getString(Script script) {
+        script.echo("Czesc swiat");
+
         return "example2"
     }
 
 
-    public void credentials() {
-        def creds = com.cloudbees.plugins.credentials.CredentialsProvider.lookupCredentials(
-                com.cloudbees.plugins.credentials.common.StandardUsernameCredentials.class,
-                Jenkins.instance,
-                null,
-                null
-        );
-        for (c in creds) {
-            println(c.id + ": " + c.description)
-        }
-    }
 }
