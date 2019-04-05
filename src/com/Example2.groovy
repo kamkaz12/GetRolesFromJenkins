641a44e76a72ddc7fda26f9070f533673ec78a67
mariusz.drobot <mariusz.drobot@accenture.com>
2019-04-05 09:30:08 +0200

    'test'

--- src/com/Example2.groovy
+++ src/com/Example2.groovy
@@ -1,36 +1,38 @@
 package com
 
 import com.michelin.cio.hudson.plugins.rolestrategy.*
+import hudson.model.Item
+import hudson.security.AuthorizationStrategy
 import hudson.security.Permission
 import jenkins.model.Jenkins
 
 class Example2 {
 
     private RoleBasedAuthorizationStrategy roleBasedAuthorizationStrategy;
-
     final String GLOBAL_ROLES_TYPE = 'globalRoles';
 
     String getString(Script script) {
         script.echo("Czesc swiat")
-        return "example2"
     }
 
-    void getRoles(String userId, String type = GLOBAL_ROLES_TYPE, Script script) {
-        final Set<Role> result = [] as Set
+    Set<Role> getRoles(String userId, String type = GLOBAL_ROLES_TYPE) {
+        if (!userId) {
+            return null
+        }
 
-        final RoleMap roleMap = roleBasedAuthorizationStrategy.getRoleMap(type);
-        final Set<Role> roles = roleMap.getRoles();
+        final Set<Role> result = [] as Set
+        final RoleMap roleMap = roleBasedAuthorizationStrategy.getRoleMap(type)
+        final Set<Role> roles = roleMap.getRoles()
 
         if (userId) {
             roles.each { role ->
                 final Set<String> sids = roleMap.getSidsForRole(role.getName())
                 if (sids.contains(userId)) {
                     result.add(role)
-                    script.echo(role.toString())
                 }
             }
         }
-
+        return result
     }
 
 
