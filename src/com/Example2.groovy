037ad3f178fcbee181b1b115377647676c3ae6d6
mariusz.drobot <mariusz.drobot@accenture.com>
2019-04-05 09:26:01 +0200

    'test'

--- src/com/Example2.groovy
+++ src/com/Example2.groovy
@@ -8,17 +8,14 @@ class Example2 {
 
     private RoleBasedAuthorizationStrategy roleBasedAuthorizationStrategy;
 
-    final Map<String, Permission> GLOBAL_ROLES_PERMISSIONS = [
-            'Administer': Jenkins.ADMINISTER,
-            'Read': Jenkins.READ
-    ]
+    final String GLOBAL_ROLES_TYPE = 'globalRoles';
 
     String getString(Script script) {
         script.echo("Czesc swiat")
         return "example2"
     }
 
-    void getRoles(String userId= "User", String type = GLOBAL_ROLES_TYPE, Script script) {
+    void getRoles(String userId, String type = GLOBAL_ROLES_TYPE, Script script) {
         final Set<Role> result = [] as Set
 
         final RoleMap roleMap = roleBasedAuthorizationStrategy.getRoleMap(type);
